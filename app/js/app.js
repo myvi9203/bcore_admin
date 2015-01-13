feec477a3054a7fab268eca28ae010a646394cc4
zulkifli <zulkifli@dagangnet.com>
2015-01-13 18:33:50 +0800

    new code

---
+++ app/js/app.js
@@ -0,0 +1,15 @@
+angular.module('F1FeederApp', [
+  'F1FeederApp.services',
+  'F1FeederApp.controllers',
+  'ngRoute'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+	when("/dashboard", {
+            templateUrl: "partials/drivers.html", controller: "driversController"
+        }).
+	when("/drivers/:id", {
+            templateUrl: "partials/driver.html", controller: "driverController"
+        }).
+	otherwise({redirectTo: '/drivers'});
+}]);
